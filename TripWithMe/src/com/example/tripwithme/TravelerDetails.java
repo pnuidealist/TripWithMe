f213c11a3ff8f6cf1fd4dfa96cee0246a65d27e5
jjy <jjy@jjytab>
2015-07-10 00:00:04 +0900

    채팅화면 추가 -메인화면의 검색 버튼 클릭 -> 여행객 목록 클릭 -> 채팅하기 클릭 -> 채팅 창 뜸

--- TripWithMe/src/com/example/tripwithme/TravelerDetails.java
+++ TripWithMe/src/com/example/tripwithme/TravelerDetails.java
@@ -3,13 +3,16 @@ package com.example.tripwithme;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class TravelerDetails extends Activity {
 
+	private Button screenBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,5 +23,19 @@ public class TravelerDetails extends Activity {
 		actionBar.hide();
 
 		findViewById(R.id.traveler_details).setBackgroundDrawable(getResources().getDrawable(R.drawable.travelerdetails_sample));
+		
+		screenBtn = (Button)findViewById(R.id.traveler_details_btn);
+		
+		screenBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent=new Intent(TravelerDetails.this, ChatActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		
+		
 	}
 }
